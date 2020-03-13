58a334ebdc67ac378a43d10cb89e61f487988290
rageshwarib <61499788+rageshwarib@users.noreply.github.com>
2020-03-13 18:21:32 +0530

    Update harmonic.sh

--- harmonic.sh
+++ harmonic.sh
@@ -4,7 +4,7 @@ number=$1
 harmonic=0
 for ((counter=1;counter<=number;counter++))
 do
-	num=`echo "scale=4; (1 / $counter)" | bc`
+   num=`echo "scale=4; (1 / $counter)" | bc`
    harmonic=`echo "scale=2; ($harmonic + $num)" | bc`
 done
 echo "Harmonic Number:: $harmonic"
