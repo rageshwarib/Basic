196accc460bc4dad1da0b9d348134e43f2425626
ShitalSonwane <nilamkurhade87@gmail.com>
2020-03-16 15:37:35 +0530

    Printing the nth Harmonic Number

---
+++ harmonic.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x	
+
+number=$1	
+harmonic=0	
+for ((counter=1;counter<=number;counter++))	
+do	
+   num=`echo "scale=4; (1 / $counter)" | bc`	
+   harmonic=`echo "scale=2; ($harmonic + $num)" | bc`	
+done	
+echo "Harmonic Number:: $harmonic"
