4a695554ade1a98e322714d748d8682534d22b96
ShitalSonwane <nilamkurhade87@gmail.com>
2020-03-16 15:40:37 +0530

    Check whether the given number is Prime Number or not

---
+++ primeCheck.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x	
+
+echo "Enter a number::"	
+read number	
+variable=0	
+
+for ((counter=1;counter<=number;counter++))	
+do	
+	if [ $(( $number % $counter )) -eq 0 ]	
+	then	
+		((variable++))	
+	fi	
+done	
+
+if [ $variable -eq 2 ]	
+then 	
+	echo "Given number is Prime Number"	
+else	
+	echo "Given number is not a Prime number"	
+fi
