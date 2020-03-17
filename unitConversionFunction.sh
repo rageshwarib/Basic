3a14dbdccc2f32c4f3bd0a4493bc5d1ce40af32c
ShitalSonwane <nilamkurhade87@gmail.com>
2020-03-17 12:49:56 +0530

    Finding degF or degC based on their Conversion selection using functions

---
+++ unitConversionFunction.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -x	
+
+minCelciusTemp=0	
+maxCelciusTemp=100	
+minFahrenheitTemp=32	
+maxFahrenheitTemp=212	
+
+function celciusToFahrenheit()	
+{	
+	read -p ">>>Enter a temperature in Celcius::" celciusTemp	
+	if (( $celciusTemp >= $minCelciusTemp && $celciusTemp <= $maxCelciusTemp ))	
+	then	
+		fahrenheitTemp=`echo "scale=3; (($celciusTemp*9/5))+$minFahrenheitTemp" | bc`	
+		echo ">>>Temperature in Fahrenheit ::" `echo "scale=3; $fahrenheitTemp" | bc`	
+	else	
+		echo "Invalid Input..."	
+	fi	
+}	
+
+function fahrenheitToCelcius()	
+{	
+   read -p ">>>Enter a temperature in Fahrenheit::" fahrenheitTemp	
+   if (( $fahrenheitTemp >= $minFahrenheitTemp && $fahrenheitTemp <= $maxFahrenheitTemp ))	
+   then	
+      celciusTemp=`echo "scale=3; (($fahrenheitTemp-$minFahrenheitTemp))*5/9" | bc`	
+      echo ">>>Temperature in Celcius ::" `echo "scale=3; $celciusTemp" | bc`	
+   else	
+      echo "Invalid Input..."	
+	fi	
+}	
+
+echo "1.Celcius to Fahrenheit"	
+echo "2.Faehrenheit to Celcius"	
+read -p "Enter your choice:" choice	
+case $choice in 	
+	1)	
+		celciusToFahrenheit ;;	
+	2)	
+		fahrenheitToCelcius ;;	
+	*)	
+		echo "Invalid choice" ;;	
+esac
