82f8783b62b914044dac09e60930be849c07790f
ShitalSonwane <nilamkurhade87@gmail.com>
2020-03-17 12:44:42 +0530

    Writing a function to check whether the two numbers are Palindromes or not

---
+++ palindromeFunction.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x	
+
+
+function isPalindrome()	
+{	
+	firstNumber=$1	
+	secondNumber=$2	
+	Number=$firstNumber	
+	while (($Number!=0))	
+   do	
+      remainder=$((Number % 10))	
+      sum=$((sum*10+remainder))	
+      Number=$((Number/10))	
+   done	
+	if (( $secondNumber == $sum ))	
+   then 	
+		echo "Given numbers are Palindromes"	
+	else	
+		echo "Given numbers are not Palindromes"	
+	fi	
+}	
+
+read -p "Enter first Number:: " firstNumber	
+read -p "Enter second Number:: " secondNumber	
+result="$( isPalindrome $firstNumber $secondNumber )"	
+echo $result
