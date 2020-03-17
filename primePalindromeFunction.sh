41d07db8daab0c47eefdf0e372c97a70fd3c90c9
ShitalSonwane <nilamkurhade87@gmail.com>
2020-03-17 12:59:05 +0530

    Checking the number is prime and showing its Palindrome is also Prime

--- primePalindromeFunction.sh
+++ primePalindromeFunction.sh
@@ -26,8 +26,8 @@ function getPalindrome()
 	while (( $temp != 0 ))	
 	do	
 		remainder=$(($temp % 10))	
-      reverse=$((reverse*10+$remainder))	
-      temp=$(($temp/10))	
+		reverse=$((reverse*10+$remainder))	
+		temp=$(($temp/10))	
 	done	
 	echo "Reverse of $1 is $reverse"	
 }	
